fbe8ff643167cb17b2ee1014a042d017bf47c7e7
JohnJohn <joaohcosoares@gmail.com>
2026-07-26 12:39:45 -0300

    change: Aplicação de linhas para tornar o código rodável e que traga resultados

--- testePennylane.py
+++ testePennylane.py
@@ -8,3 +8,10 @@ dev = qml.device("default.qubit", wires=wires) #Inicializa um dispositivo quânt
 def codifica_na_base(b): #define a função que codifica um vetor binário b em um estado quântico usando a base computacional.
     qml.BasisEmbedding(b, wires=wires) #É a porta quantica (operação) principal do circuito. 
     return qml.state() #instrui o nó quântico a retornar o vetor de estado resultante após a aplicação da codificação.
+
+# Agora vou testar a função codifica_na_base com diferentes vetores binários b.
+
+vetor_classico = 5 # Define o valor clássico de entrada
+
+estado_final = codifica_na_base(vetor_classico) # Chama a função codifica_na_base com o valor clássico definido, resultando no estado quântico final.
+print("Estado final do sistema quântico:", estado_final) # Imprime o estado final do sistema quântico, que é o vetor de estado resultante da codificação do valor clássico 5
